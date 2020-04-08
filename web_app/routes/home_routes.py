08edb860b5076c0f1169321617f783aae1f6de09
sma299 <59669463+sma299@users.noreply.github.com>
2020-04-08 17:42:37 -0400

    fixed up the registration form!

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,7 +1,7 @@
 
 # web_app/routes/home_routes.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request, flash
 
 home_routes = Blueprint("home_routes", __name__)
 
@@ -17,7 +17,18 @@ def about():
     print("VISITED THE ABOUT PAGE")
     return render_template("about.html")
 
-@home_routes.route("/register")
-def register():
+@home_routes.route("/users/new")
+def new_user():
     print("VISITED THE REGISTRATION PAGE")
-    return "Sign Up for our Product! (TODO)"
+    return render_template("registration_form.html")
+
+@home_routes.route("/users/create", methods=[POST"])    
+
+def create_user():
+    print("CREATING A NEW USER...")
+    print("FORM DATA:", dict(request.form)) #> {'full_name': 'Example User', 'email_address': 'me@example.com', 'country': 'US'}
+    user = dict(request.form)
+    # todo: store in a database or google sheet!
+    #flash(f"User '{user['full_name']}' created successfully!", "success")
+    flash(f"User '{user['full_name']}' created successfully!", "warning")
+    return redirect("/")
